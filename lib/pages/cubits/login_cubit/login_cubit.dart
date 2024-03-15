cbea77cb8111a48b76bd2ce5dd9a05bfa55e8d1c
AbdelrahmanAttef74 <abdoeldox@gmail.com>
2024-03-15 17:24:05 +0200

    create login cubit

---
+++ lib/pages/cubits/login_cubit/login_cubit.dart
@@ -0,0 +1,6 @@
+import 'package:chat_app/pages/cubits/login_cubit/login_states.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class LoginCubit extends Cubit<LoginStates> {
+  LoginCubit(super.initialState);
+}
