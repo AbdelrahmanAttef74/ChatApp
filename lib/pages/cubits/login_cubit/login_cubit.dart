1cb3f66f09cadaf01e45c5f6970c1eaddf602947
AbdelrahmanAttef74 <abdoeldox@gmail.com>
2024-03-16 01:03:45 +0200

    Refactor LoginCubit constructor for clarity
    Update LoginCubit constructor to improve readability and consistency with Flutter Bloc conventions.

--- lib/pages/cubits/login_cubit/login_cubit.dart
+++ lib/pages/cubits/login_cubit/login_cubit.dart
@@ -3,11 +3,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginCubit extends Cubit<LoginStates> {
-  LoginCubit(super.initialState);
+  LoginCubit() : super(LoginInitial());
 
   Future<void> loginUser(
       {required String email, required String password}) async {
-    // ignore: unused_local_variable
     emit(LoginLoading());
     try {
       UserCredential user = await FirebaseAuth.instance
