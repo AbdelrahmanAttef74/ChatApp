e090150553fed5bd4eadc7b11ccd44fe3effc890
AbdelrahmanAttef74 <abdoeldox@gmail.com>
2024-03-16 01:03:04 +0200

    Add LoginCubit to main.dart
    Wrap MaterialApp with BlocProvider to provide LoginCubit for state management.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:chat_app/firebase_options.dart';
 import 'package:chat_app/pages/chat_page.dart';
+import 'package:chat_app/pages/cubits/login_cubit/login_cubit.dart';
+import 'package:chat_app/pages/cubits/login_cubit/login_states.dart';
 import 'package:chat_app/pages/log_in_page.dart';
 import 'package:chat_app/pages/registor_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,14 +21,17 @@ class ChatApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        LoginPage.id: (context) => LoginPage(),
-        RegistorPage.id: (context) => const RegistorPage(),
-        ChatPage.id: (context) => ChatPage(),
-      },
-      initialRoute: LoginPage.id,
+    return BlocProvider(
+      create: (context) => LoginCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {
+          LoginPage.id: (context) => LoginPage(),
+          RegistorPage.id: (context) => const RegistorPage(),
+          ChatPage.id: (context) => ChatPage(),
+        },
+        initialRoute: LoginPage.id,
+      ),
     );
   }
 }
